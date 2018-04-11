6d6b2d651eff99b9748110125d29a6dd3610b429
Ujjal Kumar Dutta <duttaujjal143@gmail.com>
2018-04-11 15:48:34 +0530

    Added keywords in package.json

--- app/src/main.js
+++ app/src/main.js
@@ -2,9 +2,10 @@
 import Controller from './controller'
 const cli = require('commander')
 const chalk = require('chalk')
+const packageData = require('../../package.json')
 
 cli
-  .version('1.0.0')
+  .version(packageData.version, '-v, --version')
   .option('-t, --target <n>', 'Path of the target directory')
   .usage('[options] <package> [morePackage ...]')
   .parse(process.argv)
@@ -15,12 +16,6 @@ let controller = new Controller(path, cli.args)
 
 console.log(chalk.gray('\nKEEP CALM!\nNSI GOT YOU COVERED'))
 controller.run()
-
-// error handling
-process.on('FileNotFoundError', (err) => {
-  console.log(chalk.redBright(err.toString()))
-  process.exit()
-})
 
 process.on('unhandledRejection', (reason, p) => {
   console.log(chalk.red('Failed to execute command'))
