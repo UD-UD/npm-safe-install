3403de4a9c79601d9bf1630fc80fbee3b53826ca
Venryx <venryx@gmail.com>
2020-02-25 23:38:37 -0800

    * Made it so that, prior to npm install running, symlinked dependency folders get unlinked -- otherwise the npm install command deletes subdependencies in the symlinked folders that are shared by the root project. * Fixed that SymlinkCollector would keep tunneling deeper into all subfolders, when the root folder's path contains the "@" character. (now it only tunnels deeper when the folder name itself starts with "@" -- intended for packages hosted under an "@"-prefixed organization folder, apparently) * Fixed bug regarding the async/await in SymlinkCollector.execute, whereby the processing of subfolders would not be "awaited" for by the root execute call -- thus their results were being ignored.

--- app/src/collectSymlinks.js
+++ app/src/collectSymlinks.js
@@ -10,27 +10,32 @@ export default class SymlinkCollector {
   async execute (startPath, cb) {
     let pathToNodeModules
 
-    if (startPath.includes('@')) {
+    if (path.basename(startPath).startsWith('@')) {
       pathToNodeModules = startPath
     } else {
       pathToNodeModules = path.join(startPath, 'node_modules')
     }
 
     await new Promise((resolve, reject) => {
-      glob(pathToNodeModules + '/*', (err, foundPaths) => {
+      glob(pathToNodeModules + '/*', async (err, foundPaths) => {
         if (err) console.warn(err)
-        foundPaths.forEach(foundPath => {
+        for (const foundPath of foundPaths) {
           let stats = fs.lstatSync(foundPath)
-          if (stats.isDirectory()) {
-            this.execute(foundPath, this.callback)
+          if (stats.isDirectory() && path.basename(foundPath).startsWith('@')) {
+            await this.execute(foundPath, this.callback)
           } else if (stats.isSymbolicLink()) {
             this.callback(path.basename(foundPath), foundPath)
+
+            // unlink the symlinked dependency folders prior to `npm install`
+            // (else, in symlinked folders, it deletes subdeps shared by root project)
+            console.log('Unlinking symlinked dependency (protects its subdependencies): ' + path.basename(foundPath))
+            fs.unlinkSync(foundPath)
           }
-        })
+        }
         resolve()
       })
     })
-    return Promise.resolve(this.packages)
+    return this.packages
   }
 
   callback (pkgName, foundPath) {
